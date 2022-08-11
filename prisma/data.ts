bc835fc0e316951eebe588eda2496b09ddec6f16
Josh Newton <josh.newton@me.com>
2022-08-11 00:11:08 -0400

    Migrate to planetscale database

---
+++ prisma/data.ts
@@ -0,0 +1,178 @@
+export const flockData = [
+  {
+    "id": "cl6l74hjz0057u2pe8bdu24ib",
+    "name": "Newton Flock",
+    "description": "All of the chickens from the Newton compound",
+    "imageUrl": "https://firebasestorage.googleapis.com/v0/b/chicken-tracker-83ef8.appspot.com/o/Flocks%2Fsn1tikB7Xt1XZgU5ygHa.jpg?alt=media&token=45dc6a64-e3cd-4a16-a385-c63e631d8482",
+    "type": "egg-layers",
+    "userId": "cl6l183480006cgpey5xxutr7",
+    "logs": [
+      {
+        "id": "cl6l7hayr0131w1pez4og9zxy",
+        "count": 9,
+        "notes": "",
+        "date": "2022-08-08T04:00:00.000Z",
+        "flockId": "cl6l74hjz0057u2pe8bdu24ib"
+      },
+      {
+        "id": "cl6l7hayr0133w1pepl9orrsu",
+        "count": 20,
+        "notes": null,
+        "date": "2022-08-07T04:00:00.000Z",
+        "flockId": "cl6l74hjz0057u2pe8bdu24ib"
+      },
+      {
+        "id": "cl6l7hayr0135w1pex3tfvyld",
+        "count": 12,
+        "notes": null,
+        "date": "2022-08-06T04:00:00.000Z",
+        "flockId": "cl6l74hjz0057u2pe8bdu24ib"
+      },
+      {
+        "id": "cl6l7hayr0136w1pepctozowh",
+        "count": 12,
+        "notes": null,
+        "date": "2022-08-05T04:00:00.000Z",
+        "flockId": "cl6l74hjz0057u2pe8bdu24ib"
+      },
+      {
+        "id": "cl6l7hayr0138w1pey326c7h3",
+        "count": 14,
+        "notes": null,
+        "date": "2022-08-04T04:00:00.000Z",
+        "flockId": "cl6l74hjz0057u2pe8bdu24ib"
+      },
+      {
+        "id": "cl6l7hayr0139w1pe9eirwmch",
+        "count": 10,
+        "notes": null,
+        "date": "2022-08-03T04:00:00.000Z",
+        "flockId": "cl6l74hjz0057u2pe8bdu24ib"
+      },
+      {
+        "id": "cl6l7hayr0141w1pe09ip3iw2",
+        "count": 2,
+        "notes": null,
+        "date": "2022-08-02T04:00:00.000Z",
+        "flockId": "cl6l74hjz0057u2pe8bdu24ib"
+      },
+      {
+        "id": "cl6l7hayr0142w1pejx5q9ful",
+        "count": 11,
+        "notes": null,
+        "date": "2022-08-01T04:00:00.000Z",
+        "flockId": "cl6l74hjz0057u2pe8bdu24ib"
+      },
+      {
+        "id": "cl6mf5cl80311zhpe39ujlktv",
+        "count": 12,
+        "notes": "Estimate - haven't gotten eggs yet...",
+        "date": "2022-08-09T04:00:00.000Z",
+        "flockId": "cl6l74hjz0057u2pe8bdu24ib"
+      }
+    ],
+    "breeds": [
+      {
+        "id": "cl6l76wcu0115u2pebgq0v11m",
+        "name": "Barred Rock",
+        "description": "Black and white barred chickens primarily used for egg laying",
+        "count": 4,
+        "imageUrl": "https://firebasestorage.googleapis.com/v0/b/chicken-tracker-83ef8.appspot.com/o/breeds%2Fimages%2Fthumbnails%2Fplymouth-rock_200x200.jpeg?alt=media&token=5b1bb97b-876a-468b-aeb7-fd0e508eefb9",
+        "averageProduction": 4.5,
+        "flockId": "cl6l74hjz0057u2pe8bdu24ib"
+      },
+      {
+        "id": "cl6l76wcv0117u2pep97pltnk",
+        "name": "Rhode Island Red",
+        "description": "Brown chickens used primarily for egg laying.",
+        "count": 3,
+        "imageUrl": "https://firebasestorage.googleapis.com/v0/b/chicken-tracker-83ef8.appspot.com/o/breeds%2Fimages%2Fthumbnails%2Frhode-island-red_200x200.jpeg?alt=media&token=1f7ca379-08bf-409a-9a56-2614e24f2a4d",
+        "averageProduction": 4.5,
+        "flockId": "cl6l74hjz0057u2pe8bdu24ib"
+      },
+      {
+        "id": "cl6l79e5w0217u2peaqu24st0",
+        "name": "Austrolarp",
+        "description": "",
+        "count": 1,
+        "imageUrl": "https://firebasestorage.googleapis.com/v0/b/chicken-tracker-83ef8.appspot.com/o/breeds%2Fimages%2Fthumbnails%2Faustralorp_200x200.jpeg?alt=media&token=bf047568-b27a-4a67-bc79-98dc6def579d",
+        "averageProduction": 4.5,
+        "flockId": "cl6l74hjz0057u2pe8bdu24ib"
+      },
+      {
+        "id": "cl6l79e5x0219u2pelf0bg0y8",
+        "name": "Cinnamon Queen",
+        "description": "",
+        "count": 12,
+        "imageUrl": "https://firebasestorage.googleapis.com/v0/b/chicken-tracker-83ef8.appspot.com/o/breeds%2Fimages%2Fthumbnails%2Fcinnoman-queen_200x200.jpeg?alt=media&token=15beb04f-b45e-441a-959a-246838058278",
+        "averageProduction": 4.5,
+        "flockId": "cl6l74hjz0057u2pe8bdu24ib"
+      },
+      {
+        "id": "cl6l79e5x0220u2peqct9iqdz",
+        "name": "Leghorn",
+        "description": "",
+        "count": 3,
+        "imageUrl": "https://firebasestorage.googleapis.com/v0/b/chicken-tracker-83ef8.appspot.com/o/breeds%2Fimages%2Fthumbnails%2Fleghorn_200x200.jpeg?alt=media&token=c2001c89-a49a-4d03-89ef-ff220c9cf9e9",
+        "averageProduction": 4.5,
+        "flockId": "cl6l74hjz0057u2pe8bdu24ib"
+      },
+      {
+        "id": "cl6l79e5x0222u2petfntju8t",
+        "name": "Ameracauna",
+        "description": "",
+        "count": 1,
+        "imageUrl": "https://firebasestorage.googleapis.com/v0/b/chicken-tracker-83ef8.appspot.com/o/breeds%2Fimages%2Fthumbnails%2Fameraucana_200x200.jpeg?alt=media&token=2e46424f-a7c9-40f1-a00f-1fcadeb28a39",
+        "averageProduction": 4.5,
+        "flockId": "cl6l74hjz0057u2pe8bdu24ib"
+      },
+      {
+        "id": "cl6l79e5x0224u2pe9k2vxcjw",
+        "name": "Black Copper Maran",
+        "description": "",
+        "count": 1,
+        "imageUrl": "https://firebasestorage.googleapis.com/v0/b/chicken-tracker-83ef8.appspot.com/o/breeds%2Fimages%2Fthumbnails%2Fblack-copper-maran_200x200.jpeg?alt=media&token=43933cf3-5917-4d1f-ae3e-2ecbbee334ad",
+        "averageProduction": 0,
+        "flockId": "cl6l74hjz0057u2pe8bdu24ib"
+      }
+    ],
+    "owner": {
+      "id": "cl6l183480006cgpey5xxutr7",
+      "name": "Josh Newton",
+      "email": "newtonjd1@gmail.com",
+      "emailVerified": null,
+      "image": "https://lh3.googleusercontent.com/a-/AFdZucq8b1sEB4FA6uEZgGX_9GDIvSIS4ENGVV3DBl9wsa4=s96-c",
+      "defaultFlock": "cl6l74hjz0057u2pe8bdu24ib",
+      "accounts": [
+        {
+          "id": "cl6l1834g0013cgpeimr3imzi",
+          "userId": "cl6l183480006cgpey5xxutr7",
+          "type": "oauth",
+          "provider": "google",
+          "providerAccountId": "116715465747877985698",
+          "refresh_token": "1//0dOka0nvXYht9CgYIARAAGA0SNwF-L9Ir5pOktcTbUsglv1x5n5lASK9_VEuvuBWvpB57uihx2MpYK6h_-zPnmuA7yq3h67PL76I",
+          "access_token": "ya29.A0AVA9y1ssFIocdog_Egm1dvSBRdXMAer9iIKJGEUwkGTiTFXBgLl_bpKkIuUVfH7hJEFomuxG9moGhLtAiRGbAzhKTuyce26_e7dX8ncXBjSgCGoMAlIrrZ5041evp00QhvvQ2ij_PHqjCgLXpSuIjsD__GKCaCgYKATASATASFQE65dr8zAQk6nNWNHvICe4GHzxdMw0163",
+          "expires_at": 1659983585,
+          "token_type": "Bearer",
+          "scope": "https://www.googleapis.com/auth/userinfo.profile openid https://www.googleapis.com/auth/userinfo.email",
+          "id_token": "eyJhbGciOiJSUzI1NiIsImtpZCI6ImZkYTEwNjY0NTNkYzlkYzNkZDkzM2E0MWVhNTdkYTNlZjI0MmIwZjciLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIyMzkyMzAyNDA5NTEtcGgyMzhva2pzNXBscXFqNjc1bDJjZWtuN2k1cW9zYm0uYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiIyMzkyMzAyNDA5NTEtcGgyMzhva2pzNXBscXFqNjc1bDJjZWtuN2k1cW9zYm0uYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTY3MTU0NjU3NDc4Nzc5ODU2OTgiLCJlbWFpbCI6Im5ld3RvbmpkMUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6IkpKM3BMaDA4NjJpNTNUQkVkYlFmeGciLCJuYW1lIjoiSm9zaCBOZXd0b24iLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EtL0FGZFp1Y3E4YjFzRUI0RkE2dUVaZ0dYXzlHREl2U0lTNEVOR1ZWM0RCbDl3c2E0PXM5Ni1jIiwiZ2l2ZW5fbmFtZSI6Ikpvc2giLCJmYW1pbHlfbmFtZSI6Ik5ld3RvbiIsImxvY2FsZSI6ImVuIiwiaWF0IjoxNjU5OTc5OTg2LCJleHAiOjE2NTk5ODM1ODZ9.hIiMkgih-9JXLVRCFgZxelxGeNx2y90nCT2z5Isc-PV4Ujigm0tUrI4jrOWjCKNqB7rIZGlJL6LDqqZDYprQwGCmVvDfPtGzcGEY744oK-fx-SaSsJAnx_mqcWcDJpTzaXC-SyVP0v22aYLYXfHAxtI-nboBb99RpvBtD1sNCSX8FGQIEp8x5mExVO6ad0krSOia3pEueCQakaWOe_8IvVoHNiYMNP1xAu0SsBvICBolLAjqE4HLUFO0qd_Iwauy1dLPjwVycEy1cOPf1TIF71O3FmHXeV_1XLR3XtLMeHXPxYr-ZqjSHe7Wm9HOe3rZITqVSStW8i2ehO7gboqEug",
+          "session_state": null
+        },
+        {
+          "id": "cl6l2gykp0009r5pebr94472q",
+          "userId": "cl6l183480006cgpey5xxutr7",
+          "type": "oauth",
+          "provider": "facebook",
+          "providerAccountId": "10100622835824502",
+          "refresh_token": null,
+          "access_token": "EAALr2YfZBQecBACF2nh8qpbZACLNZBi5jKYPZCvzOD8zah5DvlFZAwywvAgT0uWReP7TiNMGaq8X5ObysIvLrZCQdTDqrDc0mn6tGJsbwZCvqHZBElqLdMjXiKralUoD7ac7Yyy9kLtc4U5RaR2lewVv0ZABwMWmRDZBR3sZA5t8mBY6U3wxZBrw5lVuZCI6YN4uL4EZBeCvOdNW4ZCI25fDY5J78EnEZBFbCsikHD8QuLF1LNvD3wZDZD",
+          "expires_at": 1665166077,
+          "token_type": "bearer",
+          "scope": null,
+          "id_token": null,
+          "session_state": null
+        }
+      ]
+    }
+  }
+]
