6e7bee13aae4ae2043281ea5ae72275c68d8e766
Josh Newton <josh.newton@me.com>
2022-08-12 17:20:52 -0400

    Move all styles to tailwind

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,14 @@
-import styles from "../styles/Card.module.scss"
-
-export default function Card({ title, children }: { title: string, children: any}) {
-    return (
-        <div className="card mat-elevation-z4">
-            <h5 className={styles.cardTitle}>{title}</h5>
-            <div className={styles.content}>
-                {children}
-            </div>
-        </div>
-    );
+export default function Card({
+  title,
+  children,
+}: {
+  title: string;
+  children: any;
+}) {
+  return (
+    <div className='mat-elevation-z4 p-8 my-4 mx-0 bg-white rounded-lg'>
+      <h5 className='text-stone-400 -translate-y-3 mt-0 mb-2'>{title}</h5>
+      <div className=''>{children}</div>
+    </div>
+  );
 }
