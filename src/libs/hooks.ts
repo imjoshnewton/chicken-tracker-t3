92403fcd73d9fe856228d9a60e98da94200df790
Josh Newton <josh.newton@me.com>
2022-08-17 16:57:49 -0400

    Update flock data hook to get data from new stats endpoint

--- src/libs/hooks.ts
+++ src/libs/hooks.ts
@@ -1,88 +1,64 @@
-import { useSession } from 'next-auth/react';
-import { trpc } from '../utils/trpc';
-import { useRouter } from 'next/router';
+import { useSession } from "next-auth/react";
+import { trpc } from "../utils/trpc";
+import { useRouter } from "next/router";
 
 export interface BaseUser {
-    id: string;
-    name?: string | null | undefined;
-    email?: string | null | undefined;
-    image?: string | null | undefined;
+  id: string;
+  name?: string | null | undefined;
+  email?: string | null | undefined;
+  image?: string | null | undefined;
 }
 
 // Custom hook to read  auth record and user profile doc
 export function useUserData() {
-    const { data, status } = useSession({
-        required: true
-    });
+  const { data, status } = useSession({
+    required: true,
+  });
 
-    return { user: data?.user, defaultFlock: data?.defaultFlock, status };
+  return { user: data?.user, defaultFlock: data?.defaultFlock, status };
 }
 
 // Custom hook to read  auth record and user profile doc
 export function useFlockData() {
-    const router = useRouter();
-    const { flockId, statsRange } = router.query;
-    const range = statsRange ? Number(statsRange) : 7;
+  const router = useRouter();
+  const { flockId, statsRange } = router.query;
+  const range = statsRange ? Number(statsRange) : 7;
+  const now = new Date(Date.now());
+  now.setHours(0, 0, 0, 0);
+  const today = now;
 
-    const flockData = trpc.useQuery(["flocks.getFlock", { flockId: flockId?.toString() }], {
-        enabled: !!flockId
-    });
-    const logsData = trpc.useQuery(["flocks.getStatLogs", { flockId: flockId?.toString(), limit: range }], {
-        enabled: !!flockId
-    });
+  const flockData = trpc.useQuery(
+    ["flocks.getFlock", { flockId: flockId?.toString() }],
+    {
+      enabled: !!flockId,
+    }
+  );
+  const logsData = trpc.useQuery(
+    [
+      "flocks.getStats",
+      {
+        flockId: flockId?.toString(),
+        limit: range,
+        today: today,
+      },
+    ],
+    {
+      enabled: !!flockId,
+      //   refetchInterval: 30000,
+    }
+  );
 
-    return { flockId, flock: flockData.data, logs: logsData.data, range, loading: !flockData.data };
+  console.log("LogsData: ", logsData);
+
+  return {
+    flockId,
+    flock: flockData.data,
+    stats: {
+      logs: logsData.data?.getLogs,
+      lastWeekAvg: logsData.data?.lastWeeksAvg,
+      thisWeekAvg: logsData.data?.thisWeeksAvg,
+    },
+    range,
+    loading: !flockData.data,
+  };
 }
-
-// export function useFlockData() {
-//     const router = useRouter();
-//     const { flockId } = router.query;
-
-//     const [flock, setFlock] = useState(null);
-
-//     useEffect(() => {
-//         let unsubFlock;
-
-//         if (flockId) {
-//             const flockDoc = doc(firestore, `flocks/${flockId}`);
-
-//             unsubFlock = onSnapshot(flockDoc, (doc) => {
-//                 setFlock(doc.data());
-//             });
-//         }
-//         else {
-//             setFlock(null);
-//         }
-
-//         return unsubFlock;
-//     }, [flockId])
-
-//     return { flockId, flock };
-// }
-
-// export function useLogsData() {
-//     const router = useRouter();
-//     const { flockId } = router.query;
-
-//     const [logs, setLogs] = useState(null);
-
-//     useEffect(() => {
-//         let unsubLogs;
-
-//         if (flockId) {
-//             const logCol = collection(firestore, 'logs');
-//             const q = query(logCol, where('flock', '==', flockId), orderBy('date', 'desc'), limit(7));
-
-//             unsubLogs = onSnapshot(q, (docs) => {
-//                 setLogs(docs.docs.map(doc => doc.data()));
-//             })
-//         }
-//         else {
-//             setLogs(null);
-//         }
-
-//         return unsubLogs;
-//     }, [flockId])
-
-//     return { logs };
-// }
