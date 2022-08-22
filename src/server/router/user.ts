197d01935e6b2cbd83e094c5a4b868ee80e25cfe
Josh Newton <josh.newton@me.com>
2022-08-22 11:23:16 -0400

    add route for giving user a defaultFlock

--- src/server/router/user.ts
+++ src/server/router/user.ts
@@ -1,23 +1,44 @@
-import { createRouter } from "./context";
 import { z } from "zod";
+import { createProtectedRouter } from "./protected-router";
 
-export const userRouter = createRouter()
-    .query("getUser", {
-        input: z
-            .object({
-                userId: z.string().nullish(),
-            })
-            .nullish(),
-        async resolve({ input, ctx }) {
-            if(input?.userId) {
-                return await ctx.prisma.user.findUnique({
-                    where: {
-                        id: input?.userId
-                    }
-                });
-            }
-            else {
-                return null;
-            }
-        },
-    })
+export const userRouter = createProtectedRouter()
+  .query("getUser", {
+    input: z
+      .object({
+        userId: z.string().nullish(),
+      })
+      .nullish(),
+    async resolve({ input, ctx }) {
+      if (input?.userId) {
+        return await ctx.prisma.user.findUnique({
+          where: {
+            id: input?.userId,
+          },
+        });
+      } else {
+        return null;
+      }
+    },
+  })
+  .mutation("setDefaultFlock", {
+    input: z
+      .object({
+        userId: z.string(),
+        flockId: z.string(),
+      })
+      .nullish(),
+    async resolve({ input, ctx }) {
+      if ((input?.userId, input?.flockId)) {
+        return await ctx.prisma.user.update({
+          where: {
+            id: input?.userId,
+          },
+          data: {
+            defaultFlock: input.flockId,
+          },
+        });
+      } else {
+        return null;
+      }
+    },
+  });
