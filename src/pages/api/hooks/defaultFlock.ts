2f72b9e2be1c9e3c6fdadbf61cfb9d7a3def4b8f
Josh Newton <josh.newton@me.com>
2022-10-27 14:20:03 -0400

    convert bsae64 to json again

--- src/pages/api/hooks/defaultFlock.ts
+++ src/pages/api/hooks/defaultFlock.ts
@@ -6,7 +6,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.body);
 
   const message = req.body.message;
-  const data = Buffer.from(req.body.message.data).toJSON();
+  const data = Buffer.from(req.body.message.data, "base64").toJSON();
 
   console.log("Data: ", data);
 
