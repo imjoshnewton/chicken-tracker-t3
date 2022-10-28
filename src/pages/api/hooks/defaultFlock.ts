19b69dd2a8c3796cb2e754272271bef37d6fb2f7
Josh Newton <josh.newton@me.com>
2022-10-28 13:15:24 -0400

    Implement user update logic for default flock

--- src/pages/api/hooks/defaultFlock.ts
+++ src/pages/api/hooks/defaultFlock.ts
@@ -1,8 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   console.log(req.body);
 
   const message = req.body.message;
@@ -11,6 +16,17 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   );
 
   console.log("Data: ", data);
+
+  const updatedUser = await prisma.user.update({
+    where: {
+      id: data.ownerId,
+    },
+    data: {
+      defaultFlock: data.flockId,
+    },
+  });
+
+  console.log("Updated user: ", updatedUser);
 
   res.status(200).json({
     message: "Thanks!",
