98d89a24548bdf65e6d7573ce0ff6cf222b0ad54
Josh Newton <josh.newton@me.com>
2022-12-19 12:49:53 -0500

    Remove ts lint rule exception

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,6 @@ import { Toaster } from "react-hot-toast";
 import type { ReactElement, ReactNode } from "react";
 import { type NextPage } from "next";
 
-// eslint-disable-next-line @typescript-eslint/ban-types
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
 };
