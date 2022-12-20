0dbfdfc5c1866d29957b70e8aad78cc804a69b08
Josh Newton <josh.newton@me.com>
2022-12-20 14:10:18 -0500

    Add notification logic to endpoint

--- src/pages/api/createSummaryNotifications.ts
+++ src/pages/api/createSummaryNotifications.ts
@@ -1,22 +1,38 @@
 import { verifySignature } from "@upstash/qstash/nextjs";
+import { subMonths } from "date-fns";
 import { type NextApiRequest, type NextApiResponse } from "next";
-
-import { getServerAuthSession } from "../../server/common/get-server-auth-session";
+import { prisma } from "../../server/db/client";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const session = await getServerAuthSession({ req, res });
+  const today = new Date();
+  const monthNum = subMonths(today, 1).getMonth();
+  const monthString = monthNum < 10 ? `0${monthNum}` : monthNum.toString();
+  const monthName = subMonths(today, 1).toLocaleString("default", {
+    month: "long",
+  });
+  const year = subMonths(today, 1).getFullYear();
+  const yearString = year.toString();
 
-  if (session) {
-    res.send({
-      content:
-        "This is protected content. You can access this content because you are signed in.",
-    });
-  } else {
-    res.send({
-      error:
-        "You must be signed in to view the protected content on this page.",
-    });
-  }
+  const flocks = await prisma.flock.findMany();
+
+  const newNotifications = await Promise.all(
+    flocks.map((flock) => {
+      return prisma.notification.create({
+        data: {
+          title: `Your ${monthName} summary is ready!`,
+          message: `Check out your flock stats for ${monthName}.`,
+          link: `https://chicken-tracker-t3.vercel.app/app/flocks/${flock.id}/summary?month=${monthString}&year=${yearString}`,
+          userId: flock.userId,
+        },
+      });
+    })
+  );
+
+  console.log("New Notifications: ", newNotifications);
+
+  res.status(200).json({
+    notifications: newNotifications,
+  });
 };
 
 export default verifySignature(handler);
