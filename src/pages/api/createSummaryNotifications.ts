8ccfb261efe4c3920ce07b8b7239c76269327dc9
Josh Newton <josh.newton@me.com>
2022-12-21 13:55:06 -0500

    Change links to be relative instead of containing domain

--- src/pages/api/createSummaryNotifications.ts
+++ src/pages/api/createSummaryNotifications.ts
@@ -26,7 +26,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         data: {
           title: `Your ${monthName} summary is ready!`,
           message: `Check out your flock stats for ${monthName}.`,
-          link: `https://chicken-tracker-t3.vercel.app/app/flocks/${flock.id}/summary?month=${monthString}&year=${yearString}`,
+          link: `/app/flocks/${flock.id}/summary?month=${monthString}&year=${yearString}`,
           userId: flock.userId,
           readDate: null,
         },
