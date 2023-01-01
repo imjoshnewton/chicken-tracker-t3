4dc1ced36a134faa3b776e85b5ad826591ab3095
Josh Newton <josh.newton@me.com>
2022-12-31 23:13:16 -0500

    Start home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import SiteLayout from "../layouts/SiteLayout";
 import { type NextPageWithLayout } from "./_app";
@@ -5,7 +6,40 @@ import { type NextPageWithLayout } from "./_app";
 const Home: NextPageWithLayout = () => {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <div className="container flex flex-col items-center justify-center py-24">
+          <div className="flex max-w-5xl flex-col items-center justify-center gap-4">
+            <h1>Welcome to FlockNerd!</h1>
+            <p className="max-w-3xl text-center">
+              Are you a backyard chicken, duck, or quail farmer looking to track
+              egg production and expenses for your flock? Look no further.
+              FlockNerd is the perfect app for you.
+            </p>
+          </div>
+        </div>
+        <div className="container flex flex-col items-center justify-center gap-12">
+          <div className="flex max-w-5xl items-center justify-center gap-10">
+            {/* <Image src="" /> */}
+            <div className="flex flex-col">
+              <p className="pb-4">
+                With FlockNerd, you can easily track the number of eggs laid by
+                your birds, as well as record expenses such as feed costs,
+                supplements and medications. Plus, you'll get access to helpful
+                statistics about your flock, including average egg production
+                per week, and total expenses.
+              </p>
+              <p className="pb-4">
+                But FlockNerd isn't just for egg farmers. It's also a great tool
+                for farmers who raise poultry for meat or who simply keep birds
+                as pets.
+              </p>
+              <p>
+                So why wait? Sign up for FlockNerd today and take the first step
+                towards better managing your poultry farm or backyard flock.
+              </p>
+            </div>
+          </div>
+        </div>
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
