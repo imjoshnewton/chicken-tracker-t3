eb364461be867d69cd341554c491e3c5960d5cbb
Josh Newton <josh.newton@me.com>
2023-06-17 18:44:26 -0400

    organize imports

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,7 +1,7 @@
 import { useSession } from "next-auth/react";
 import { trpc } from "../utils/trpc";
-import { useRouter } from "next/router";
 import { Session } from "next-auth";
+import { useRouter } from "next/router";
 
 //s
 // Custom hook to get user's session data
