c5a1ee82a61ff63f01a148a1f4a5005bfc462845
Josh Newton <josh.newton@me.com>
2023-06-19 17:58:47 -0400

    Refactor flock component and custom hook for flock data

--- src/app/app/flocks/[flockId]/Flock.tsx
+++ src/app/app/flocks/[flockId]/Flock.tsx
@@ -3,9 +3,11 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useCallback } from "react";
 import { MdOutlineEdit } from "react-icons/md";
+
+import type { Session } from "next-auth";
 
 import Breeds from "@components/breeds/Breeds";
 import ExpenseModal from "@components/flocks/ExpenseModal";
@@ -13,123 +15,159 @@ import LogModal from "@components/flocks/LogModal";
 import Stats from "@components/flocks/Stats";
 import Card from "@components/shared/Card";
 import Loader from "@components/shared/Loader";
+
 import { useFlockDataAppDir } from "@lib/hooks";
-import { Session } from "next-auth";
+import { usePathname, useSearchParams } from "next/navigation";
 
 const Flock = ({ session, flockId }: { session: Session; flockId: string }) => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const path = usePathname();
-  const breedFilter = searchParams?.get("breedFilter");
-  const statsRange = searchParams?.get("statsRange") || "7";
   const { flock, stats, range, breedStats } = useFlockDataAppDir(
     session,
     flockId,
-    statsRange,
-    breedFilter == null ? undefined : breedFilter
+    searchParams?.get("statsRange") || "7",
+    searchParams?.get("breedFilter")
   );
 
   const onRangeChange = useCallback(
     (event: any) => {
-      const newRange = event.target.value;
-      const curParams = new URLSearchParams(searchParams ? searchParams : "");
+      const curParams = new URLSearchParams(searchParams || "");
 
-      curParams.set("statsRange", newRange.toString());
+      curParams.set("statsRange", event.target.value.toString());
 
       router.replace(`${path}?${curParams.toString()}`);
     },
-    [router]
+    [router, searchParams, path]
   );
 
   const clearFilter = useCallback(() => {
-    const curParams = new URLSearchParams(searchParams ? searchParams : "");
+    const curParams = new URLSearchParams(searchParams || "");
     curParams.delete("breedFilter");
 
     router.replace(`${path}?${curParams.toString()}`);
-  }, [router]);
+  }, [router, searchParams, path]);
 
   const filterBreed = flock?.breeds.find(
-    (breed) => breed.id == (breedFilter as string)
+    (breed) => breed.id == (searchParams?.get("breedFilter") as string)
   );
-  const filterText = filterBreed?.name || filterBreed?.breed || undefined;
 
   if (!flock) {
-    return (
-      <main className="flex h-full flex-col justify-center p-0 lg:p-8 lg:px-[3.5vw]">
-        <div className="flex h-full items-center justify-center">
-          <Loader show={true}></Loader>
-        </div>
-      </main>
-    );
+    return <LoaderLayout />;
   }
 
   return (
-    <main className="flex flex-col justify-center p-0 lg:p-8 lg:px-[3.5vw]">
-      <div className="shadow-xl">
-        <Card
-          title="Flock Details"
-          className="pb-safe py-0 lg:pt-5 lg:pb-4"
-          key={flockId?.toString()}
-        >
-          <Link
-            href={`/app/flocks/${flockId}/edit`}
-            className="absolute top-0 right-0 mt-3 mr-5 flex items-center p-3 text-stone-400 transition-colors hover:cursor-pointer hover:text-stone-700 dark:hover:text-stone-200 md:mt-2"
-          >
-            Edit&nbsp;&nbsp;
-            <MdOutlineEdit />
-          </Link>
-          <motion.div
-            initial={{ opacity: 0, translateY: 10 }}
-            animate={{ opacity: 1, translateY: 0 }}
-            transition={{ duration: 0.5 }}
-            className="flex flex-wrap items-center"
-          >
-            <Image
-              src={flock?.imageUrl}
-              width="150"
-              height="150"
-              className="flock-image aspect-square object-cover"
-              alt="A user uploaded image that represents this flock"
-            />
-            <div className="ml-0 md:ml-6">
-              <div className="flex items-center">
-                <h1 className="mr-3 dark:text-gray-300">{flock?.name}</h1>
-              </div>
-              <p className="description dark:text-gray-300">
-                {flock?.description}
-              </p>
-              <p className="mt-2 text-gray-400 dark:text-gray-400">
-                {flock?.type}
-              </p>
-            </div>
-            <div className="ml-0 mt-4 flex w-full flex-wrap self-start lg:ml-auto lg:mt-0 lg:w-auto">
-              <LogModal flockId={flockId?.toString()} />
-              <div className="p-1"></div>
-              <ExpenseModal flockId={flockId?.toString()} />
-            </div>
-          </motion.div>
-          <div className="divider my-6 dark:border-t-gray-500"></div>
-          <Stats
-            stats={stats}
-            flock={flock}
-            className="basis-full xl:basis-[75%]"
-            limit={range.toString()}
-            onRangeChange={onRangeChange}
-            filter={filterText}
-            filterId={breedFilter as string}
-            clearFilter={clearFilter}
-          ></Stats>
-          <Breeds
-            flockId={flockId?.toString()}
-            breeds={flock?.breeds}
-            top={breedStats?.at(0)?.breedId}
-            className="mt-4 basis-full xl:basis-[23%]"
-            user={session?.user}
-          ></Breeds>
-        </Card>
-      </div>
-    </main>
+    <FlockLayout
+      flock={flock}
+      flockId={flockId}
+      onRangeChange={onRangeChange}
+      range={range}
+      clearFilter={clearFilter}
+      filterText={filterBreed?.name || filterBreed?.breed}
+      filterId={searchParams?.get("breedFilter") as string}
+      stats={stats}
+      breedStats={breedStats}
+      session={session}
+    />
   );
 };
+
+const LoaderLayout = () => (
+  <main className="flex h-full flex-col justify-center p-0 lg:p-8 lg:px-[3.5vw]">
+    <div className="flex h-full items-center justify-center">
+      <Loader show={true} />
+    </div>
+  </main>
+);
+
+const FlockLayout = ({
+  flock,
+  flockId,
+  onRangeChange,
+  range,
+  clearFilter,
+  filterText,
+  filterId,
+  stats,
+  breedStats,
+  session,
+}: any) => (
+  <main className="flex flex-col justify-center p-0 lg:p-8 lg:px-[3.5vw]">
+    <div className="shadow-xl">
+      <Card
+        title="Flock Details"
+        className="pb-safe py-0 lg:pt-5 lg:pb-4"
+        key={flockId?.toString()}
+      >
+        <EditLink flockId={flockId} />
+        <FlockInfo flock={flock} flockId={flockId} />
+        <Stats
+          stats={stats}
+          flock={flock}
+          className="mt-4 basis-full xl:basis-[75%]"
+          limit={range.toString()}
+          onRangeChange={onRangeChange}
+          filter={filterText}
+          filterId={filterId}
+          clearFilter={clearFilter}
+        />
+        <Breeds
+          flockId={flockId?.toString()}
+          breeds={flock?.breeds}
+          top={breedStats?.at(0)?.breedId}
+          className="mt-4 basis-full xl:basis-[23%]"
+          user={session?.user}
+        />
+      </Card>
+    </div>
+  </main>
+);
+
+const EditLink = ({ flockId }: any) => (
+  <Link
+    href={`/app/flocks/${flockId}/edit`}
+    className="absolute top-0 right-0 mt-3 mr-5 flex items-center p-3 text-stone-400 transition-colors hover:cursor-pointer hover:text-stone-700 dark:hover:text-stone-200 md:mt-2"
+  >
+    Edit&nbsp;&nbsp;
+    <MdOutlineEdit />
+  </Link>
+);
+
+const FlockInfo = ({ flock, flockId }: any) => (
+  <motion.div
+    initial={{ opacity: 0, translateY: 10 }}
+    animate={{ opacity: 1, translateY: 0 }}
+    transition={{ duration: 0.5 }}
+    className="flex flex-wrap items-center"
+  >
+    <Image
+      src={flock?.imageUrl}
+      width="150"
+      height="150"
+      className="flock-image aspect-square object-cover"
+      alt="A user uploaded image that represents this flock"
+    />
+    <FlockDetails flock={flock} />
+    <Actions flockId={flockId} />
+  </motion.div>
+);
+
+const FlockDetails = ({ flock }: any) => (
+  <div className="ml-0 md:ml-6">
+    <div className="flex items-center">
+      <h1 className="mr-3 dark:text-gray-300">{flock?.name}</h1>
+    </div>
+    <p className="description dark:text-gray-300">{flock?.description}</p>
+    <p className="mt-2 text-gray-400 dark:text-gray-400">{flock?.type}</p>
+  </div>
+);
+
+const Actions = ({ flockId }: any) => (
+  <div className="ml-0 mt-4 flex w-full flex-wrap self-start lg:ml-auto lg:mt-0 lg:w-auto">
+    <LogModal flockId={flockId?.toString()} />
+    <div className="p-1"></div>
+    <ExpenseModal flockId={flockId?.toString()} />
+  </div>
+);
 
 export default Flock;
--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,9 +1,11 @@
 import { useSession } from "next-auth/react";
-import { trpc } from "../utils/trpc";
-import { Session } from "next-auth";
 import { useRouter } from "next/router";
 
-//s
+import type { Session } from "next-auth";
+
+import { trpc } from "../utils/trpc";
+
+//
 // Custom hook to get user's session data
 //
 export function useUserData() {
@@ -106,79 +108,94 @@ export function useFlockDataAppDir(
   session: Session,
   flockId: string,
   statsRange: string,
-  breedFilter: string | undefined
+  breedFilter: string | null | undefined
 ) {
   const data = session;
   const range = statsRange ? Number(statsRange) : 7;
 
-  const now = new Date(Date.now());
-  now.setHours(0, 0, 0, 0);
-  const today = now;
+  const today = setStartOfDay(new Date());
 
-  const {
-    data: flockData,
-    error: flockError,
-    isLoading: flockLoading,
-  } = trpc.flocks.getFlock.useQuery(
-    { flockId: flockId as string },
-    {
-      enabled: !!flockId && !!data?.user,
-    }
+  const flockData = useFlockQuery(flockId, data?.user);
+  const logsData = useStatsQuery(
+    flockId,
+    range,
+    today,
+    breedFilter,
+    data?.user
   );
-  const {
-    data: logsData,
-    error: logsError,
-    isLoading: logsLoading,
-  } = trpc.stats.getStats.useQuery(
-    {
-      flockId: flockId as string,
-      limit: range,
-      today: today,
-      breedFilter: typeof breedFilter == "string" ? [breedFilter] : breedFilter,
-    },
-    {
-      enabled: !!flockId && !!range && !!today && !!data?.user,
-    }
-  );
-  const {
-    data: expenseData,
-    isLoading: expensesLoading,
-    isError: expensesError,
-  } = trpc.stats.getExpenseStats.useQuery(
-    { today: today, flockId: flockId as string },
-    {
-      enabled: !!flockId && !!range && !!today && !!data?.user,
-    }
-  );
-  const {
-    data: breedStats,
-    isLoading: breedStatsLoading,
-    isError: breedStatsError,
-  } = trpc.stats.getBreedStats.useQuery({
-    today: today,
-    flockId: flockId as string,
-  });
+  const expenseData = useExpenseStatsQuery(flockId, today, data?.user);
+  const breedStats = useBreedStatsQuery(flockId, today);
 
   return {
     flockId,
-    flock: flockData,
+    flock: flockData.data,
     stats: {
-      expenses: expenseData,
-      logs: logsData?.getLogs,
-      lastWeekAvg: logsData?.lastWeeksAvg,
-      thisWeekAvg: logsData?.thisWeeksAvg,
+      expenses: expenseData.data,
+      logs: logsData.data?.getLogs,
+      lastWeekAvg: logsData.data?.lastWeeksAvg,
+      thisWeekAvg: logsData.data?.thisWeeksAvg,
     },
     range,
-    breedStats: breedStats,
+    breedStats: breedStats.data,
     loading:
-      flockLoading && logsLoading && expensesLoading && breedStatsLoading,
+      flockData.isLoading &&
+      logsData.isLoading &&
+      expenseData.isLoading &&
+      breedStats.isLoading,
     error: {
-      flock: flockError,
-      stats: logsError,
-      expenses: expensesError,
-      breedStats: breedStatsError,
+      flock: flockData.error,
+      stats: logsData.error,
+      expenses: expenseData.error,
+      breedStats: breedStats.error,
     },
   };
+}
+
+function setStartOfDay(date: Date) {
+  date.setHours(0, 0, 0, 0);
+  return date;
+}
+
+function useFlockQuery(flockId: string, user: any) {
+  return trpc.flocks.getFlock.useQuery(
+    { flockId },
+    {
+      enabled: !!flockId && !!user,
+    }
+  );
+}
+
+function useStatsQuery(
+  flockId: string,
+  range: number,
+  today: Date,
+  breedFilter: string | null | undefined,
+  user: any
+) {
+  return trpc.stats.getStats.useQuery(
+    {
+      flockId,
+      limit: range,
+      today,
+      breedFilter: typeof breedFilter == "string" ? [breedFilter] : undefined,
+    },
+    {
+      enabled: !!flockId && !!range && !!today && !!user,
+    }
+  );
+}
+
+function useExpenseStatsQuery(flockId: string, today: Date, user: any) {
+  return trpc.stats.getExpenseStats.useQuery(
+    { today, flockId },
+    {
+      enabled: !!flockId && !!today && !!user,
+    }
+  );
+}
+
+function useBreedStatsQuery(flockId: string, today: Date) {
+  return trpc.stats.getBreedStats.useQuery({ today, flockId });
 }
 
 export function useAllFlocks() {
