10748989f122db10e75ba69326c702dde86586ab
Josh Newton <josh.newton@me.com>
2023-06-20 12:07:38 -0400

    log error

--- src/pages/api/summaryCleanup.ts
+++ src/pages/api/summaryCleanup.ts
@@ -35,6 +35,8 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
     return res.status(200).json({ message: "Files deleted successfully" });
   } catch (error) {
+    console.error(error);
+
     return res
       .status(500)
       .json({ error: "Failed to delete files", details: error });
