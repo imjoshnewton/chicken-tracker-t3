aed4bb9a7c6726a8e7eb0d3d844df300d47cf080
Josh Newton <josh.newton@me.com>
2023-06-20 12:28:27 -0400

    JSON parse the firebase private key

--- src/pages/api/summaryCleanup.ts
+++ src/pages/api/summaryCleanup.ts
@@ -4,7 +4,9 @@ import { type NextApiRequest, type NextApiResponse } from "next";
 
 const firebaseConfig = {
   projectId: process.env.FIREBASE_PROJECT_ID,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY
+    ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
+    : undefined,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 };
 
