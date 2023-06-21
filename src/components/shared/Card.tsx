d37ce45e6df0d84e6ab38f896011831bb8ca4420
Josh Newton <josh.newton@me.com>
2023-06-21 14:43:16 -0400

    Adjust classname logic for Card main and add ability to customize title styles

--- src/components/shared/Card.tsx
+++ src/components/shared/Card.tsx
@@ -2,18 +2,24 @@ export default function Card({
   title,
   children,
   className,
+  titleStyle,
 }: {
   title: string;
   children: any;
   className?: string;
+  titleStyle?: string;
 }) {
   return (
     <div
-      className={`${
+      className={`relative rounded-lg bg-[#FEF9F6] pl-8 pr-8 pb-8 pt-6 dark:bg-gray-900 ${
         className ? className : ""
-      } relative rounded-lg bg-[#FEF9F6] p-8 pt-6 dark:bg-gray-900`}
+      }`}
     >
-      <h5 className="mt-0 mb-4 text-stone-400">{title}</h5>
+      <h5
+        className={"mt-0 mb-4 text-stone-400 " + (titleStyle ? titleStyle : "")}
+      >
+        {title}
+      </h5>
       <div className="">{children}</div>
     </div>
   );
