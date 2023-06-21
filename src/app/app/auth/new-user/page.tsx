01fb7b24f74515811232b6939a8d52a9cd27f969
Josh Newton <josh.newton@me.com>
2023-06-21 16:04:19 -0400

    Fix new-user page

--- src/app/app/auth/new-user/page.tsx
+++ src/app/app/auth/new-user/page.tsx
@@ -1,9 +1,9 @@
-import Card from "../../../components/shared/Card";
+import Card from "../../../../components/shared/Card";
 import { getServerSession } from "next-auth";
 import { authOptions } from "src/pages/api/auth/[...nextauth]";
 import { redirect } from "next/navigation";
-import NewUserForm from "../settings/NewUserForm";
-import FlockForm from "../flocks/FlockEditForm";
+import NewUserForm from "../../settings/NewUserForm";
+import FlockForm from "../../flocks/FlockEditForm";
 
 export default async function NewUser() {
   const session = await getServerSession(authOptions);
@@ -14,10 +14,10 @@ export default async function NewUser() {
 
   if (!session.user.image && !session.user.name) {
     return (
-      <main>
+      <main className="flex flex-col justify-center p-0 lg:p-8 lg:px-[3.5vw]">
         <div className="shadow-xl">
           <Card title="New user info">
-            <div className="flex">
+            <div className="flex flex-col">
               <section>
                 <h2 className="mb-3">Welcome!</h2>
                 <p className="mb-3">
@@ -36,14 +36,18 @@ export default async function NewUser() {
   }
 
   return (
-    <main>
+    <main className="flex flex-col justify-center p-0 lg:p-8 lg:px-[3.5vw]">
       <div className="shadow-xl">
-        <Card title="New flock">
+        <Card
+          title="New flock"
+          className="pb-safe h-full !pl-0 !pr-0 lg:h-auto lg:pt-4 lg:pb-0"
+          titleStyle="pl-8 !mb-0"
+        >
           <div className="flex flex-col">
-            <section>
+            <section className="p-8">
               <h2 className="mb-3">Welcome!</h2>
               <p className="mb-3">
-                Let&apos;s make a new flock.
+                Let&apos;s make a new flock&nbsp;🐓🦆🦃
                 <br />
                 Give your flock a name, description (optional), and select
                 whether they are egg layers or meat birds.
