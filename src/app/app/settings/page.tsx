851359a2ad777e4d766f3eabf7e4c2f75cf45d20
Josh Newton <josh.newton@me.com>
2023-09-07 09:44:18 -0400

    Add clerk user profile to settings page

--- src/app/app/settings/page.tsx
+++ src/app/app/settings/page.tsx
@@ -1,27 +1,16 @@
-import Card from "../../../components/shared/Card";
-import { getServerSession } from "next-auth";
-import { authOptions } from "src/app/api/auth/[...nextauth]/route";
-import { redirect } from "next/navigation";
-import NewUserForm from "./NewUserForm";
+import { UserProfile } from "@clerk/nextjs";
 
 export const metadata = {
   title: "FlockNerd - Account Settings",
   description: "Flock Stats for Nerds",
 };
 
+export const runtime = "edge";
+
 const Settings = async () => {
-  const session = await getServerSession(authOptions);
-
-  if (!session?.user) redirect("/api/auth/signin");
-
   return (
     <main className="h-full p-0 lg:h-auto lg:p-8 lg:px-[3.5vw]">
-      <Card
-        title="User Settings"
-        className="pb-safe animate__animated animate__fadeIn h-full py-0 lg:h-auto lg:pt-4 lg:pb-4"
-      >
-        <NewUserForm user={session.user} />
-      </Card>
+      <UserProfile />
     </main>
   );
 };
