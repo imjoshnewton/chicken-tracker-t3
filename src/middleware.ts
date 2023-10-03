eba53f1fdcf08fb1f53ea05aeb28eae8df707682
Josh Newton <josh.newton@me.com>
2023-10-02 21:43:48 -0400

    most basic middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,14 +3,15 @@ import { authMiddleware } from "@clerk/nextjs";
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
-export default authMiddleware({
-  publicRoutes: [
-    "/",
-    "/api/summaryCleanup",
-    "/api/createSummaryNotifications",
-    "/api/webhooks/user",
-  ],
-});
+export default authMiddleware();
+// {
+// publicRoutes: [
+//   "/",
+//   "/api/summaryCleanup",
+//   "/api/createSummaryNotifications",
+//   "/api/webhooks/user",
+// ],
+// });
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
