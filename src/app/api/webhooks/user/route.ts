7c164ff081a6cd7f6e027de0685af878e5800155
Josh Newton <josh.newton@me.com>
2023-10-07 21:54:31 -0400

    Update webhook to use drizzle

--- src/app/api/webhooks/user/route.ts
+++ src/app/api/webhooks/user/route.ts
@@ -1,8 +1,11 @@
-import { prisma } from "../../../../server/db/client";
 import type { IncomingHttpHeaders } from "http";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook, WebhookRequiredHeaders } from "svix";
+import { db } from "@lib/db";
+import { user as Users } from "@lib/db/schema";
+import cuid from "cuid";
+import { eq } from "drizzle-orm";
 
 const webhookSecret = "whsec_+SfGI52GxrWmhHBZfEGRajXHCDTIFE7a";
 
@@ -41,21 +44,15 @@ async function handler(request: Request) {
     const image = attributes.image_url as string;
 
     // console.log({ clerkId, name, email, image });
-
-    await prisma.user.upsert({
-      where: { clerkId: id as string },
-      create: {
-        clerkId,
-        name,
-        email,
-        image,
-      },
-      update: {
-        name,
-        email,
-        image,
-      },
-    });
+    if (eventType === "user.created") {
+      const id = cuid();
+      await db.insert(Users).values({ id, clerkId, name, email, image });
+    } else {
+      await db
+        .update(Users)
+        .set({ clerkId, name, email, image })
+        .where(eq(Users.clerkId, clerkId));
+    }
   }
 
   return NextResponse.json({}, { status: 200 });
